09a8419fe319e2d53f870cdd5c7b61cd00dc0688
Muhammad Abdullah <muhammad0abdullah68@gmail.com>
2026-07-06 16:29:42 +0500

    called check_reboot from main, and exist with 1 if didn't pending

--- all_checks.py
+++ all_checks.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import os
+import sys
 
 def check_reboot():
 	"""Return True if their is any pending reboots."""
 	return os.path.path("/run/reboot-required")
 
 def main():
-	pass
+	if check_reboot():
+		print("Reboot Pending!")
+	sys.exit(1)
 
 main()
 
